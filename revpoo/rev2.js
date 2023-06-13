e70ca3dcf62a536bbb0b20567226fa804b930d44
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-13 13:40:05 -0300

    revisao

---
+++ revpoo/rev2.js
@@ -0,0 +1,7 @@
+//o usuario vai digitar 3 numeros
+//1- mostrar o somatorio destes numeros
+// 2 - mostrar qual foi o maior numero digitado
+// 3 o progama para de solicitar numeros quando 
+//4 o progama para de solicitar numeros quando
+//o usuario digitar um numero negativo
+// o soma dos numeros não dever considerar o numero negativo 
